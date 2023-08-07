983a2f76d82adafc377576b4edceccb24f932c6d
Abhay1509 <abhayjaiswal333333.aj@gmail.com>
2023-08-07 23:05:33 +0530

    5_Connected to mongodb

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,6 +1,35 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const port = 8080;
 
-//req contains all data for the request....server<-client
-//res contains all data for the response....server<-db
-//comment
+// Connection to mongo db database
+mongoose
+  .connect(
+    "mongodb+srv://chaudhary_abhay:" +
+      process.env.MONGO_PASSWORD +
+      "@cluster0.rdkhsnv.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((x) => {
+    console.log("Connected to mongo");
+  })
+  .catch((err) => {
+    console.log("Error");
+  });
+
+//API : get type
+app.get("/", (req, res) => {
+  //req contains all data for the request....server<-client
+  //res contains all data for the response....server<-db
+
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log("App is running on port" + port);
+});
