561378b76842dea75b7064edb73485b058c1a8ad
Abhay1509 <abhayjaiswal333333.aj@gmail.com>
2023-08-12 01:31:07 +0530

    12-song routes

--- spotify_backend/routes/song.js
+++ spotify_backend/routes/song.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const Song = require("../models/Song");
+const User = require("../models/User");
 
 router.post(
   "/create",
@@ -29,6 +30,33 @@ router.get(
     const currentUser = req.user;
     // We need to get all songs where artist id == currentUser._id
     const songs = await Song.find({ artist: req.user._id }).exec();
+    return res.status(200).json({ data: songs });
+  }
+);
+
+// Get route to get all songs any artist has published
+// I will send an artist id and I want to see all songs that artist has published
+router.get(
+  "/get/artist",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { artistId } = req.body;
+    const artist = await User.find({ _id: artistId }); //We can check if the artist does not exist
+    if (!artist) {
+      return res.status(301).json({ err: "Artist does not exist" });
+    }
+    const songs = await Song.find({ artist: artistId }).exec();
+    return res.status(200).json({ data: songs });
+  }
+);
+
+// Get route to get a single song by name
+router.get(
+  "/get/songname",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { songName } = req.body;
+    const songs = await Song.find({ name: songName }).exec();
     return res.status(200).json({ data: songs });
   }
 );
