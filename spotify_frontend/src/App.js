2c8dfe5fb6702d6b3e23ed6dae6244016fae8a55
Abhay1509 <abhayjaiswal333333.aj@gmail.com>
2023-08-15 04:37:09 +0530

    23-Login and Signup page done

--- spotify_frontend/src/App.js
+++ spotify_frontend/src/App.js
@@ -1,14 +1,16 @@
 import "./output.css";
 import LoginComponent from "./routes/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignupComponent from "./routes/Signup";
 
 function App() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen font-poppins">
       <BrowserRouter>
         <Routes>
           <Route path="bye" element={<div className="bg-blue-500">bye</div>} />
           <Route path="login" element={<LoginComponent />} />
+          <Route path="/signup" element={<SignupComponent />} />
         </Routes>
       </BrowserRouter>
     </div>
