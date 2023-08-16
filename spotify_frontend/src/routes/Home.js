328cefe44e884492102151624ec0989a7e09decc
Abhay1509 <abhayjaiswal333333.aj@gmail.com>
2023-08-16 06:51:40 +0530

    28-Home page done

--- spotify_frontend/src/routes/Home.js
+++ spotify_frontend/src/routes/Home.js
@@ -1,6 +1,40 @@
 import spotify_logo from "../assets/images/spotify_logo_white.svg";
 import IconText from "../components/shared/IconText";
 import { Icon } from "@iconify/react";
+import TextWithHover from "../components/shared/TextWithHover";
+
+const focusCardsData = [
+  {
+    title: "Peaceful Piano",
+    description: "Relax and indulge with beautiful piano pieces",
+    imgUrl:
+      "https://images.unsplash.com/photo-1470019693664-1d202d2c0907?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBlYWNlJTIwcGlhbm98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    title: "Deep Focus",
+    description: "Keep calm and focus with this music",
+    imgUrl:
+      "https://images.unsplash.com/photo-1502710655568-ee441b74b0b9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Zm9jdXMlMjBtdXNpY3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    title: "Instrumental Study",
+    description: "Focus with soft study music in the background.",
+    imgUrl:
+      "https://images.unsplash.com/photo-1564186763535-ebb21ef5277f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGluc3RydW1lbnRhbCUyMG11c2ljfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    title: "Focus Flow",
+    description: "Up tempo instrumental hip hop beats",
+    imgUrl:
+      "https://images.unsplash.com/uploads/1411160110892ab555b6f/80b0d25e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9jdXMlMjBtdXNpY3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    title: "Beats to think to",
+    description: "Focus with deep techno and tech house",
+    imgUrl:
+      "https://images.unsplash.com/photo-1530352865347-3c2e277abefe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YmVhdHMlMjBtdXNpY3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
 
 const Home = () => {
   return (
@@ -42,7 +76,60 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="h-full"></div>
+      <div className="h-full w-4/5 bg-app-black overflow-auto">
+        <div className="navbar w-full h-1/10 bg-black bg-opacity-40 flex items-center justify-end">
+          <div className="w-1/2 flex h-full">
+            <div className="w-3/5 flex justify-around items-center">
+              <TextWithHover displayText="Premium" />
+              <TextWithHover displayText="Support" />
+              <TextWithHover displayText="Download" />
+              <div className="h-1/2 border-r border-white"></div>
+            </div>
+            <div className="w-2/5 flex justify-around h-full items-center">
+              <TextWithHover displayText="Sign up" />
+              <div className="bg-white h-2/3 px-8 flex items-center justify-center rounded-full font-semibold cursor-pointer">
+                Log in
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="content p-8 pt-0 overflow-auto">
+          <PlaylistView titleText="Focus" cardsData={focusCardsData} />
+          <PlaylistView titleText="Spotify Playlists" cardsData={focusCardsData}/>
+          <PlaylistView titleText="Sound of India" cardsData={focusCardsData}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const PlaylistView = ({ titleText, cardsData }) => {
+  return (
+    <div className="text-white mt-8">
+      <div className="text-2xl font-semibold mb-5">{titleText}</div>
+      <div className="w-full flex justify-between space-x-4">
+        {cardsData.map((item) => {
+          return (
+            <Card
+              title={item.title}
+              description={item.description}
+              imgUrl={item.imgUrl}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Card = ({ title, description, imgUrl }) => {
+  return (
+    <div className="bg-black bg-opacity-40 w-1/5 p-4 rounded-lg">
+      <div className="pb-4 pt-2">
+        <img className="w-full rounded-md" src={imgUrl} alt="label" />
+      </div>
+      <div className="text-white font-semibold py-3">{title}</div>
+      <div className="text-gray-500 text-sm">{description}</div>
     </div>
   );
 };
